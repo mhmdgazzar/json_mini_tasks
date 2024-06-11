1858d805feeae2b3fc0788ea36f310413726d40d
mhmdgazzar <mhmdgazzar@gmail.com>
2024-06-11 16:36:21 +0200

    task 3

--- tasks/task3.dart
+++ tasks/task3.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import '../domain/user.dart';
+
 void main() {
   String json3 = """
 {
@@ -15,4 +19,13 @@ void main() {
 """;
 
   // Erstelle eine Instanz der Klasse User basierend auf dem JSON-String json3
+  Map<String, dynamic> m3 = jsonDecode(json3);
+  User user3 = User(
+      gender: 'unknown',
+      fullName: (m3['person']['name']['first'] + m3['person']['name']['last']),
+      city: m3['person']['city']);
+
+  print('Gender: ${user3.gender}');
+  print('Fullname: ${user3.fullName}');
+  print('City: ${user3.city}');
 }
